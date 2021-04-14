21f1c7f643c1b78cbb9ba62cd43fabef602a01d8
iswaryacs <reach2ishucs@gmail.com>
2021-04-14 10:51:36 +0530

    First day task

---
+++ src/main/java/org/gittask/Samplegittask.java
@@ -0,0 +1,13 @@
+package org.gittask;
+
+public class Samplegittask {
+	public void bank() {
+		System.out.println("Axis bank");
+
+	}
+	public static void main(String[] args) {
+		Samplegittask sample= new Samplegittask();
+		sample.bank();
+	}
+
+}
